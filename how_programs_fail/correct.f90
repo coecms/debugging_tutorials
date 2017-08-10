45f81da4bd2b3006a64adc82876ffa200ef387de
Holger Wolff <holger.wolff@monash.edu>
2017-08-10 14:22:51 +1000

    wrote random data to file data.txt

--- how_programs_fail/correct.f90
+++ how_programs_fail/correct.f90
@@ -2,15 +2,18 @@ program correct
 
   implicit none
   integer :: s, n
-  integer :: ios
+  integer :: ios, u
 
   s = 0
 
+  open(newunit=u, file='data.txt', status='old', action='read', iostat=ios)
+  if (ios /= 0) STOP "error opening file"
   do
-    read(*, *, iostat=ios) n
+    read(u, *, iostat=ios) n
     if (ios /= 0) exit
     s = s + n
   end do
+  close(u)
 
   print*, s
 
--- how_programs_fail/hang.f90
+++ how_programs_fail/hang.f90
@@ -2,14 +2,17 @@ program hang
 
   implicit none
   integer :: s, n
-  integer :: ios
+  integer :: ios, u
 
   s = 0
 
+  open(newunit=u, file='data.txt', status='old', action='read', iostat=ios)
+  if (ios /= 0) STOP "error opening file"
   do
-    read(*, *, iostat=ios) n
+    read(u, *, iostat=ios) n
     s = s + n
   end do
+  close(u)
 
   print*, s
 
