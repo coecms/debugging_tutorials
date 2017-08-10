7bf60882ce6e1d95263596cb4da4f70fbcd1ebec
Holger Wolff <holger.wolff@monash.edu>
2017-08-10 14:14:00 +1000

    added hang.f90

---
+++ how_programs_fail/hang.f90
@@ -0,0 +1,16 @@
+program correct
+
+  implicit none
+  integer :: s, n
+  integer :: ios
+
+  s = 0
+
+  do
+    read(*, *, iostat=ios) n
+    s = s + n
+  end do
+
+  print*, s
+
+end program correct
