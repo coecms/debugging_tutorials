5536eb6c5154b358b962f7615fd414c5b9bcecfc
Holger Wolff <holger.wolff@monash.edu>
2017-08-23 12:17:45 +1000

    modified for intel compiler

--- debug_stacktrace/mod_three.f90
+++ debug_stacktrace/mod_three.f90
@@ -2,7 +2,9 @@ module mod_three
     implicit none
 contains
     subroutine three()
+        use ifcore
         implicit none
+        call tracebackqq
         call abort
     end subroutine three
 end module mod_three
