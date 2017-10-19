e54b13625032c7afa5cd43f47245d4de0e192699
Holger Wolff <holger.wolff@monash.edu>
2017-10-19 13:57:56 +1100

    added traceback to faculty routine

--- stackframe/stackframe.F90
+++ stackframe/stackframe.F90
@@ -13,10 +13,12 @@ program stackframe
 contains
 
     recursive function fact(n) result (f)
+        use ifcore, only: tracebackqq
         integer, intent(in) :: n
         integer :: f
         if (n == 1) then
             f = 1
+            call tracebackqq
         else
             f = n * fact(n-1)
         end if
