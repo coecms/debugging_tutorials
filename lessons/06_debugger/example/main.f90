c35e541a359446474e1db7f4d536064385af0501
Holger Wolff <holger.wolff@monash.edu>
2017-10-20 09:31:36 +1100

    moved function calls out of print statement

--- lessons/06_debugger/example/main.f90
+++ lessons/06_debugger/example/main.f90
@@ -4,11 +4,14 @@ program main
     use mod_fib
     implicit none
     integer :: n
+    integer :: fib_n, fac_n
 
     print *, "Please enter a number: "
     read *, n
 
-    print*, "faculty of n is ", fac(n)
-    print*, "fib of n is ", fib(n)
+    fac_n = fac(n)
+    print*, "faculty of n is ", fac_n
+    fib_n = fib(n)
+    print*, "fib of n is ", fib_n
 
 end program main
