afe824344783cdbe3f59289caafb1ace07c1c917
Holger Wolff <holger.wolff@monash.edu>
2017-10-20 09:32:01 +1100

    made fib routine recursive

--- lessons/06_debugger/example/mod_fib.f90
+++ lessons/06_debugger/example/mod_fib.f90
@@ -3,21 +3,22 @@ module mod_fib
     implicit none
 contains
 
-    function fib(n)
+    recursive function fib(n) result(f)
         implicit none
         integer, intent(in) :: n
-        integer :: fib
-        integer :: a, b, i
+        integer :: f
+!        integer :: a, b, i
         if ( n <= 2 ) then
-            fib = 1
+            f = 1
         else
-            a = 1
-            b = 1
-            do i = 3, n
-                fib = a+b
-                a = b
-                b = fib
-            end do
+            f = fib(n-1) + fib(n-2)
+!            a = 1
+!            b = 1
+!            do i = 3, n
+!                f = a+b
+!                a = b
+!                b = f
+!            end do
         end if
     end function fib
 
