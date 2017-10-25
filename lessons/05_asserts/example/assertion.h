9939c5b23152d800d584277be752fad4a9aec656
Holger Wolff <holger.wolff@monash.edu>
2017-10-25 11:33:13 +1100

    divided assertion up into separate files

--- lessons/05_asserts/example/assertion.h
+++ lessons/05_asserts/example/assertion.h
@@ -1,16 +1,4 @@
 #ifdef DEBUG
-
-subroutine assertion(expr, note)
-    use ifcore
-    implicit none
-    logical, intent(in) :: expr
-    character(len=*), intent(in) :: note
-    if (expr) return
-    print *, note
-    call tracebackqq
-    call abort
-end subroutine assertion
-
 #define assert(expr, note) call assertion(expr, note)
 #else
 #define assert(expr, note)
