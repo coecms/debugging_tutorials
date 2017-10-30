d604066a7522289ae3bfcff23b17dcb753d62cca
Holger Wolff <holger.wolff@monash.edu>
2017-10-31 10:18:55 +1100

    added qsort as debug exercise

---
+++ lessons/07_debugger/exercise/mod_qsort.f90
@@ -0,0 +1,50 @@
+module mod_qsort
+    implicit none
+
+contains
+
+    recursive subroutine my_qsort(a)
+        implicit none
+        integer, intent(inout) :: a(:)
+        integer :: lower_idx, upper_idx
+        integer :: pivot, divider, tmp
+        if (size(a) <= 1) return
+        lower_idx = 2
+        upper_idx = size(a)
+        pivot = a(1)
+        do
+            do while (lower_idx < size(a) .and. a(lower_idx) <= pivot)
+                lower_idx = lower_idx + 1
+            end do
+            do while (upper_idx > 1 .and. a(upper_idx) > pivot)
+                upper_idx = upper_idx - 1
+            end do
+            if (lower_idx < upper_idx) then
+                call swap(a, lower_idx, upper_idx)
+            else
+                exit
+            end if
+        end do
+        if (lower_idx == 2) then
+            ! the first element is the smallest, need a different pivot
+            call swap(a, 1, 2)
+            call my_qsort(a)
+            return
+        end if
+        divider = upper_idx
+        call swap(a, 1, divider)
+        if (divider > 2) call my_qsort(a(:divider-1))
+        if (divider < size(a)-2) call my_qsort(a(divider+1:))
+    end subroutine my_qsort
+
+    subroutine swap(a, i, j)
+        implicit none
+        integer, intent(inout) :: a(:)
+        integer, intent(in) :: i, j
+        integer :: tmp
+        tmp = a(i)
+        a(i) = a(j)
+        a(j) = tmp
+        return
+    end subroutine swap
+end module mod_qsort
--- lessons/07_debugger/exercise/qsort.f90
+++ lessons/07_debugger/exercise/qsort.f90
@@ -1,42 +1,14 @@
 program debug_qsort
 
+    use mod_qsort, only : my_qsort
+
     implicit none
     integer :: a(20)
 
     a = [17, 4, 9,15, 1, 3, 2,13,14,10, 5,18,16, 6,11,12, 8, 0, 7,19]
+    print '(*(I3))', a
     call my_qsort(a)
 
-    print '(10(I3))', a
-
-contains
-
-    recursive subroutine my_qsort(a)
-        implicit none
-        integer, intent(inout) :: a(:)
-        integer :: lower_idx, upper_idx
-        integer :: pivot, tmp
-        if (size(a) == 1) return
-        lower_idx = 2
-        upper_idx = size(a)
-        pivot = a(1)
-        do
-            if (a(lower_idx) <= pivot) then
-                lower_idx = lower_idx + 1
-            else
-                tmp = a(upper_idx)
-                a(upper_idx) = a(lower_idx)
-                a(lower_idx) = tmp
-                upper_idx = upper_idx - 1
-            end if
-            if (upper_idx == lower_idx) exit
-        end do
-        if (a(lower_idx) <= pivot) then
-            call my_qsort(a(:lower_idx))
-            call my_qsort(a(lower_idx+1:))
-        else
-            call my_qsort(a(:lower_idx-1))
-            call my_qsort(a(lower_idx:))
-        end if
-    end subroutine my_qsort
+    print '(*(I3))', a
 
 end program debug_qsort
