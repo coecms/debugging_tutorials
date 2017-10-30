9846618f39285b3a131a430b2b4dca7062962844
Holger Wolff <holger.wolff@monash.edu>
2017-10-30 11:16:07 +1100

    fixed unintended bug with mod_power.f90

--- lessons/06_unit_tests/exercise/mod_power.f90
+++ lessons/06_unit_tests/exercise/mod_power.f90
@@ -5,7 +5,7 @@ contains
     function is_power_of_two(n)
         integer, intent(in) :: n
         logical :: is_power_of_two
-        is_power_of_two = (iand(n, n-1) == 0)
+        is_power_of_two = (iand(n, n-1) == 0) .and. (n /= 0)
         return
     end function is_power_of_two
 
